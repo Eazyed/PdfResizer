450b36f51ee3d544ef37b09678903f24cc6a69b8
Mathias Ruelle <mathias.ruelle42@gmail.com>
2021-05-21 12:13:46 +0200

    Lecture des metadata

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const sharp = require('sharp');
+const mysql = require('mysql');
 const { Worker } = require('worker_threads')
 const readline = require('readline');
 
@@ -7,11 +8,31 @@ const shell = readline.createInterface({
     output: process.stdout
 });
 
-shell.question("Entrez un mot clé : ", (keyword) => {
-    console.log(`Vous avez entré le mot clé ${keyword}`);
-    shell.close();
-});
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "imageresizer"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+        shell.question("Entrez un mot clé : ", (keyword) => {
+            console.log(`Vous avez entré le mot clé ${keyword}`);
+            keyword = `%${keyword}%`;
+            var sql = 'SELECT image_path FROM projects WHERE metadata like ? ';
+            con.query(sql, [keyword], function (err, result) {
+                if (err) throw err;
+                console.log(result);
+            });
+        });    
+  });
+
+
 
 shell.on('close', () => {
     console.log("Au revoir");
 });
+
--- workers/fileExplorerWorker.js
+++ workers/fileExplorerWorker.js
@@ -1,6 +1,15 @@
 const { parentPort, workerData } = require('worker_threads');
 const fs = require('fs');
-const CONFIG = require('../config.json')
+const sharp = require('sharp');
+const CONFIG = require('../config.json');
+const mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "imageresizer"
+  });
 
 /**
  * Parcourt le dossier spécifié dans workerData
@@ -12,9 +21,31 @@ async function run() {
             console.error("Something bad happened in thread " + workerData.name);
         } else {
             files.forEach(file => {
+                
                 if (CONFIG.fileExtensions.includes(file.split('.')[1])) {
+                    let metadata ='';
                     console.log("[" + workerData.name + "]" + file);
+                    fs.readFile(`${folderpath}${file.split('.')[0]}.metadata`, 'utf8' , (err, data) => {
+                        if (err) {
+                          console.error(err);
+                          return
+                        }
+                        console.log(data);
+                        metadata = data;
+                      })
+                    
+                } else if (file.split('.')[1]=='metadata')
+                {
+                    // Logique liée au fichier source
+                } else {
+                    const oldPath = `${folderpath}${file}`;
+                    const newPath = `${CONFIG.pathToTrash}${file}`;
+                    fs.rename(oldPath,newPath, function (err) {
+                        if (err) throw err
+                        console.log(`moved ${file} to trash`)
+                      });
                 }
+
             });
             console.log(workerData.name + " finished.");
         }
